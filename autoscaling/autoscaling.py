3b882778aeaedea837d051a862127470afca72b3
fayasAK <msfayas001@gmail.com>
2020-05-16 21:16:56 +0530

    lamda function created

--- autoscaling/autoscaling.py
+++ autoscaling/autoscaling.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 # Boto Connection
-#please enter your aws_region, autoscaling-group-name,min,max and desired capacity
+# aws_region, autoscaling-group-name,min,max and desired capacity has to be passed
 asg = boto3.client('autoscaling','os.environ[aws_region]')
 def lambda_handler(event, context):
   response = asg.update_auto_scaling_group(AutoScalingGroupName=os.environ['asg_name'],MinSize=os.environ['min'],DesiredCapacity=os.environ['desired'],MaxSize=os.environ['max'])
